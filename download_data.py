870553e0de9373039a8aff7dc0c2588c9457f225
unknown <jonas.o.angulski@gmail.com>
2024-04-10 16:33:49 -0300

    chore: scripts for domain

--- download_data.py
+++ download_data.py
@@ -7,9 +7,16 @@ import concurrent.futures
 
 url = 'https://dados.rfb.gov.br/CNPJ/'
 
-
+#Lists for estabelecimentos anchors and download url's
 urls = []
 estabelecimentos = []
+
+#Lists for support files anchors and download url's
+urls_support = []
+support_anchor = []
+
+#List of words to find in anchors
+support = ['motivos','municipios','paises']
 
 content = requests.get(url)
 
@@ -20,7 +27,7 @@ soup = BeautifulSoup(content.text,'html')
 def download_file(url, filename):
     response = requests.get(url)
     if response.status_code == 200:
-        with open(f'data/raw/{filename}', 'wb') as file:
+        with open(f'{filename}', 'wb') as file:
             file.write(response.content)
         print(f"File downloaded successfully as '{filename}'")
     else:
@@ -39,12 +46,35 @@ for i in estabelecimentos:
     url = f'https://dados.rfb.gov.br/CNPJ/{i}'
     urls.append(url)
 
-# get name of files in lowercase
-filenames = [lower.lower() for lower in estabelecimentos]
+#Get support files anchors 
+for anchor in soup.find_all('a'):
+    for sup in support:
+        if sup in anchor.text.lower():
+            support_anchor.append(anchor['href'])
+        else:
+            pass
+
+#Get support files urls
+for i in support_anchor:
+    url = f'https://dados.rfb.gov.br/CNPJ/{i}'
+    urls_support.append(url)
+
+#Name and directory for estabelecimentos files
+filenames_estabele = ['data/raw/'+estabe.lower() for estabe in estabelecimentos]
+
+#Name and directory for support file
+filenames_support = ['data/raw/support/' + sup + '.zip' for sup in support]
 
 #Define number of threads
-num_threads = min(len(urls),10)
+num_threads_estabe = min(len(urls),10)
+num_threads_supp = min(len(urls),3)
 
 # Download files using multiple threads
-with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-    executor.map(download_file, urls, filenames)    
+with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads_estabe) as executor:
+    executor.map(download_file, urls, filenames_estabele)    
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads_supp) as executor:
+    executor.map(download_file, urls_support, filenames_support)
+
+
--- extract_data.py
+++ extract_data.py
@@ -2,13 +2,23 @@ import os
 import zipfile
 
 #Files in raw folder
-files = os.listdir('data/raw')
+files_esta = os.listdir('data/raw')
+files_sup = os.listdir('data/raw/support')
 
 #extract each file and dump into extract folder
-for file in files:
+for file in files_esta:
     if file.endswith('.zip'):
         file_name = file.split('.')[0]
         file_path = os.path.join('data/raw',file)
         with zipfile.ZipFile(file_path,'r') as zip_ref:
-            zip_ref.extractall(f'data/raw/extract/{file_name}')
+            zip_ref.extractall(f'data/bronze/extract/{file_name}')
+        print('Done')
+
+#extract each file and dump into extract folder
+for file in files_sup:
+    if file.endswith('.zip'):
+        file_name = file.split('.')[0]
+        file_path = os.path.join('data/raw/support',file)
+        with zipfile.ZipFile(file_path,'r') as zip_ref:
+            zip_ref.extractall(f'data/bronze/support/{file_name}')
         print('Done')
